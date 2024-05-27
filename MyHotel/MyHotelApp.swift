c210f4f9f1244b48f4337955ba869864084da483
Matthew Collins <matthew.d.collins101@gmail.com>
2024-05-27 04:22:50 -0400

    Initial Commit

---
+++ MyHotel/MyHotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHotelApp.swift
+//  MyHotel
+//
+//  Created by Matthew Collins on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MyHotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
