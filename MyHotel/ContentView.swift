792a8b70cf2b04bee7dc026f40c3f4394534df6e
Matthew Collins <matthew.d.collins101@gmail.com>
2024-05-27 04:52:46 -0400

    testing

--- MyHotel/ContentView.swift
+++ MyHotel/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "square.and.arrow.up")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
